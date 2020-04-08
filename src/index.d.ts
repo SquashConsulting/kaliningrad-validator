acd9c83004af5faa3dc01042e88b9706fa4b3d91
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2020-04-08 15:46:14 +0400

    Add JSDoc

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,7 @@
+/**
+ * Validates given graph data structure
+ *
+ * @param data Kaliningrad Graph Structure
+ * @returns `true` or `false`
+ */
 export default function validate(data: object): PromiseLike<any> | boolean;
